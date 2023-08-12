01fb65f695aa5e90fa5b4d1e1e46bec1b52bc5f2
o2alex <af@O2.services>
2023-08-12 16:38:05 -0700

    fixed an issue with too many tokens in the AI request

--- metagpt/actions/action.py
+++ metagpt/actions/action.py
@@ -8,12 +8,16 @@
 from abc import ABC
 from typing import Optional
 
-from tenacity import retry, stop_after_attempt, wait_fixed
+from openai.error import APIConnectionError
+from tenacity import retry, stop_after_attempt, wait_exponential_jitter, wait_fixed, after_log, retry_if_exception_type
+from tiktoken import Encoding
 
 from metagpt.actions.action_output import ActionOutput
 from metagpt.llm import LLM
 from metagpt.utils.common import OutputParser
 from metagpt.logs import logger
+
+import tiktoken
 
 class Action(ABC):
     def __init__(self, name: str = '', context=None, llm: LLM = None):
@@ -44,9 +48,14 @@ class Action(ABC):
         if not system_msgs:
             system_msgs = []
         system_msgs.append(self.prefix)
-        return await self.llm.aask(prompt, system_msgs)
 
-    @retry(stop=stop_after_attempt(2), wait=wait_fixed(1))
+        return await self._call_llm_aask(prompt, system_msgs)
+
+    async def _aask_no_prefix(self, prompt: str, system_msgs: list[str]) -> str:
+        """Append default prefix"""
+        return await self._call_llm_aask(prompt, system_msgs)
+
+
     async def _aask_v1(self, prompt: str, output_class_name: str,
                        output_data_mapping: dict,
                        system_msgs: Optional[list[str]] = None) -> ActionOutput:
@@ -54,13 +63,65 @@ class Action(ABC):
         if not system_msgs:
             system_msgs = []
         system_msgs.append(self.prefix)
-        content = await self.llm.aask(prompt, system_msgs)
+        content = await self._call_llm_aask(prompt, system_msgs)
         logger.debug(content)
         output_class = ActionOutput.create_model_class(output_class_name, output_data_mapping)
         parsed_data = OutputParser.parse_data_with_mapping(content, output_data_mapping)
         logger.debug(parsed_data)
         instruct_content = output_class(**parsed_data)
         return ActionOutput(content, instruct_content)
+
+
+    def tokens_count(self, enc: Encoding, prompt: str, system_msgs: list[str]) -> int:
+        strNewLine = '\n'
+        allText = f'''
+            
+            {strNewLine.join(system_msgs)}
+            
+            {prompt}
+            
+            '''
+        tokens = enc.encode(allText, allowed_special='all')
+        return len(tokens)
+
+
+#    @retry(stop=stop_after_attempt(10), wait=wait_exponential_jitter(initial=1))
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(1),
+        after=after_log(logger, logger.level('WARNING').name),
+        retry=retry_if_exception_type(APIConnectionError),
+        #retry_error_callback=log_and_reraise,
+    )
+    async def _call_llm_aask(self, prompt: str, system_msgs: list[str]) -> str:
+        enc = tiktoken.encoding_for_model("gpt-4")
+        while self.tokens_count(enc, prompt, system_msgs) > 8000-3000:
+            system_msgs = system_msgs[1:]
+
+        strNewLine = '\n'
+        strAll = f'''
+{strNewLine.join(system_msgs)}
+
+{prompt}
+'''
+        text_len = len(strAll)
+        logger.info(f'''
+
+00000000000000000000000000000000
+
+_call_llm_aask len: {text_len}
+
+system_msgs:
+{strNewLine.join(system_msgs)}
+
+prompt:
+{prompt}
+
+00000000000000000000000000000000
+
+''')
+        return await self.llm.aask(prompt, system_msgs)
+
 
     async def run(self, *args, **kwargs):
         """Run action"""
